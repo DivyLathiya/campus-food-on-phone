3aee3c432d51c67f17382283ec8da13a4b28a31d
Le-strange <drown1804@gmail.com>
2025-09-24 18:20:15 +0530

    updated

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,11 +6,12 @@ plugins {
 }
 
 android {
-    namespace = "com.example.campus_food_app"
-    compileSdk = flutter.compileSdkVersion
+    namespace = "com.example.testing"
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -21,11 +22,11 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.campus_food_app"
+        applicationId = "com.example.testing"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
@@ -41,4 +42,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
